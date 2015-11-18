119d5dff1a9faeac9271bb17af429f104858f583
Joel Efah <JEfah@spartaglobal.com>
2015-11-18 16:06:20 +0000

    program can store and return phone numbers

--- person.rb
+++ person.rb
@@ -1,12 +1,13 @@
 require "date"
 class Person
-    attr_accessor :dob, :first_name, :surname, :emails
+    attr_accessor :dob, :first_name, :surname, :emails, :phone_numbers
 	def initialize (fname, sname, dob)
 		@dob = Date.parse(dob)
 		#capatilize the first letter of string
 		@first_name = fname.capitalize
 		@surname = sname.capitalize
 		@emails = []
+		@phone_numbers = []
 	end
 	
 	def full_name
@@ -15,7 +16,9 @@ class Person
 
 	def add_email(emails)
 		@emails << emails
-		
 	end
 
+	def add_phone(phone_numbers)
+		@phone_numbers << phone_numbers
+	end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -12,11 +12,20 @@ describe "Person Class - entriies in phonebook" do
  	end
  	it "should store and return email information" do #name of unit test
 		person = Person.new("joe", "bloggs", "21-09-1992")
-		
+
 		expect(person.emails).to eq [] #
 		person.add_email "joe@foo.com" #
 		person.add_email "joe.bloggs@work.com" #
 		expect(person.emails).to eq ["joe@foo.com", "joe.bloggs@work.com"] #
+	end
+
+	it "should store and return phone numbers" do
+		person = Person.new("joe", "bloggs", "21-09-1992")
+
+		expect(person.phone_numbers).to eq []
+		person.add_phone "07712345678"
+		person.add_phone "02012345678"
+		expect(person.phone_numbers).to eq ["07712345678", "02012345678"]
 	end
 end
 
