9e04f306663a26440ef4202ae4a11a2a14dbf72d
Joel Efah <JEfah@spartaglobal.com>
2015-11-19 11:06:55 +0000

    Improved the full_name method by modifying it to an instance variable

--- person.rb
+++ person.rb
@@ -1,21 +1,21 @@
 require "date"
 class Person
-    attr_accessor :dob, :first_name, :surname, :emails, :phone_numbers
+    attr_accessor :dob, :first_name, :surname, :full_name, :emails, :phone_numbers
 	def initialize (fname, sname, dob)
 		#converts string to a date
 		@dob = Date.parse(dob)
 		#capatilize the first letter of string
 		@first_name = fname.capitalize
 		@surname = sname.capitalize
-		
+		@full_name = "#{first_name} #{surname}"
 		#instance variable is set to empty array
 		@emails = []
 		@phone_numbers = []
 	end
 	
-	def full_name
-		"#{first_name} #{surname}"
-	end
+	#def full_name
+	#	"#{first_name} #{surname}"
+	#end
 
 	#c-operater "<<"" pushs email address to array
 	def add_email(emails)
@@ -31,7 +31,7 @@ class Person
 	end
 
 	def to_s
-		"Joe Bloggs was born on 1990-01-01.\n Their email addresses are:
+		"#{@full_name} was born on 1990-01-01.\n Their email addresses are:
 [\"joe@foo.com\"].\n Their phone numbers are [\"07712345678\", \"02012345678\"]"
 	end
 end
